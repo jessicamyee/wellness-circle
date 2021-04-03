d348fcb39a40103df4660910dd71bb281fea3021
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-04-03 16:19:15 -0400

    initial styling for inner circle screen

--- client/src/screens/InnerCircle.jsx
+++ client/src/screens/InnerCircle.jsx
@@ -1,25 +1,25 @@
 import { Link } from "react-router-dom";
 import SummaryRow from "../components/SummaryRow";
+import "../screens-css/InnerCircle.css";
+
 
 export default function InnerCircle(props) {
   const { allShares } = props;
 
   return (
-    <div>
-      <h1>My Inner Circle</h1>
-      <Link to="/user_shares/list">
-        <button>View / Modify Your Shared Settings</button>
-      </Link>
-      {allShares.map((individualAverageData) => {
-        return (
-          <div key={individualAverageData.id}>
-            {individualAverageData.fullName}
-            <SummaryRow
-              personalAverageData={individualAverageData}
-            />
-          </div>
-        );
-      })}
+    <div className="inner-circle-container">
+        <h1 className="inner-circle-title">My Inner Circle</h1>
+        <Link to="/user_shares/list">
+          <button id="share-settings-btn">View / Modify Your Shared Settings</button>
+        </Link>
+        {allShares.map((individualAverageData) => {
+          return (
+            <div key={individualAverageData.id}>
+              {individualAverageData.fullName}
+              <SummaryRow personalAverageData={individualAverageData} />
+            </div>
+          );
+        })}
     </div>
   );
 }
