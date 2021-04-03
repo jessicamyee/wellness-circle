d58aa3fa7a7685ebfce6fa34ec01a296aa8e3513
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-04-03 13:37:00 -0400

    add styling for login screen

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Nav from "../components/Nav/Nav";
-import Footer from "../components/Footer/Footer"
+import Footer from "../components/Footer/Footer";
+import "./Layout.css";
 
 export default function Layout(props) {
   const { currentUser, handleLogout } = props;
   return (
-    <div>
+    <>
       <Nav currentUser={currentUser} handleLogout={handleLogout} />
-      {props.children}
-      <Footer/>
-    </div>
+      <div className="layout-main-content">{props.children}</div>
+      <Footer />
+    </>
   );
 }
--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "../screens-css/Login.css";
 
 export default function Login(props) {
   const [formData, setFormData] = useState({
@@ -18,35 +19,42 @@ export default function Login(props) {
   };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleLogin(formData);
-      }}
-    >
-      <h3>Login</h3>
-      <label>
-        Username:
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <Link to="/signup">Sign Up</Link>
-      <button>Submit</button>
-    </form>
+    <div className='login-form-container'>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleLogin(formData);
+        }}
+      >
+        <h3 id="login-title">Log In</h3>
+        <label className="login-text">
+          Username:
+          <input
+            className="login-field"
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label className="login-text">
+          Password:
+          <input
+            className="login-field"
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <div>
+          <button id="login-btn">Log In</button>
+        </div>
+        <div id="redirect-signup-from-login">
+          <Link to="/signup">Don’t have an account? Sign up here!</Link>
+        </div>
+      </form>
+    </div>
   );
 }
