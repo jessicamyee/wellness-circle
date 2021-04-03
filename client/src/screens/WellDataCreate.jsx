fe85c3245b5cdfb720faa3645ab6ee9e2b181a8a
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-04-03 14:52:54 -0400

    record wellness screen styling

--- client/src/screens/WellDataCreate.jsx
+++ client/src/screens/WellDataCreate.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import "../screens-css/WellDataCreate.css";
 
 export default function WellDataCreate(props) {
-
   const [formData, setFormData] = useState({
     socialScore: "",
     physicalScore: "",
@@ -31,107 +31,116 @@ export default function WellDataCreate(props) {
   };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleCreate(formData);
-      }}
-    >
-      <h1>Record Today</h1>
-      <h3>
-        From 1-10, how would you rate the following categories for today?{" "}
-      </h3>
-      <div>
-        <label>
-          Social:
-          <input
-            type="number"
-            id="number"
-            name="socialScore"
-            min="1"
-            max="10"
-            value={socialScore}
-            onChange={handleChange}
-          />
-        </label>
-      </div>
+    <div className="create-form-container">
+      <h1 id="create-title">Record Today</h1>
+      <form
+        className="create-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleCreate(formData);
+        }}
+      >
+        <h3 id="create-statement">
+          From 1-10, how would you rate the following categories for today?{" "}
+        </h3>
+        <div>
+          <label className="create-text">
+            Social:
+            <input
+              className="create-field"
+              type="number"
+              id="number"
+              name="socialScore"
+              min="1"
+              max="10"
+              value={socialScore}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
-      <div>
-        <label>
-          Physical:
-          <input
-            type="number"
-            id="number"
-            name="physicalScore"
-            min="1"
-            max="10"
-            value={physicalScore}
-            onChange={handleChange}
-          />
-        </label>
-      </div>
+        <div>
+          <label className="create-text">
+            Physical:
+            <input
+              className="create-field"
+              type="number"
+              id="number"
+              name="physicalScore"
+              min="1"
+              max="10"
+              value={physicalScore}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
-      <div>
-        <label>
-          Emotional:
-          <input
-            type="number"
-            id="number"
-            name="emotionalScore"
-            min="1"
-            max="10"
-            value={emotionalScore}
-            onChange={handleChange}
-          />
-        </label>
-      </div>
+        <div>
+          <label className="create-text">
+            Emotional:
+            <input
+              className="create-field"
+              type="number"
+              id="number"
+              name="emotionalScore"
+              min="1"
+              max="10"
+              value={emotionalScore}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
-      <div>
-        <label>
-          Intellectual:
-          <input
-            type="number"
-            id="number"
-            name="intellectualScore"
-            min="1"
-            max="10"
-            value={intellectualScore}
-            onChange={handleChange}
-          />
-        </label>
-      </div>
+        <div>
+          <label className="create-text">
+            Intellectual:
+            <input
+              className="create-field"
+              type="number"
+              id="number"
+              name="intellectualScore"
+              min="1"
+              max="10"
+              value={intellectualScore}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
-      <div>
-        <label>
-          Occupational:
-          <input
-            type="number"
-            id="number"
-            name="occupationalScore"
-            min="1"
-            max="10"
-            value={occupationalScore}
-            onChange={handleChange}
-          />
-        </label>
-      </div>
+        <div>
+          <label className="create-text">
+            Occupational:
+            <input
+              className="create-field"
+              type="number"
+              id="number"
+              name="occupationalScore"
+              min="1"
+              max="10"
+              value={occupationalScore}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
-      <div>
-        <label>
-          Spiritual:
-          <input
-            type="number"
-            id="number"
-            name="spiritualScore"
-            min="1"
-            max="10"
-            value={spiritualScore}
-            onChange={handleChange}
-          />
-        </label>
-      </div>
+        <div>
+          <label className="create-text">
+            Spiritual:
+            <input
+              className="create-field"
+              type="number"
+              id="number"
+              name="spiritualScore"
+              min="1"
+              max="10"
+              value={spiritualScore}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
-      <button>Submit</button>
-    </form>
+        <button id="add-wellness-btn">Submit</button>
+      </form>
+    </div>
   );
 }
