780af34ee37dd058440cab3d267f8a1c86a108b9
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-04-03 09:56:31 -0400

    separate login and sign up

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,34 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 export default function Nav(props) {
   const { currentUser, handleLogout } = props;
 
   return (
     <div>
-      <header>
-        <Link to='/'><h1>Wellness Circle</h1></Link>
-        {
-          currentUser ?
-            <>
-              <p>{currentUser.username}</p>
-              <button onClick={handleLogout}>Logout</button>
-            </>
-            :
-            <Link to='/login'>Login/Sign Up</Link>
-        }
-      </header>
-      
+      <Link to="/">
+        <h1>Wellness Circle</h1>
+      </Link>
+
       {currentUser && (
         <>
-          <p>{currentUser.first_name}</p>
-          <Link to='/wellness_data/new'>Record Today</Link>
-          <Link to='/wellness_data'>Dashboard</Link>
-          <Link to='/user_shares'>My Inner Circle</Link>
-          <hr />
+          <Link to="/wellness_data/new">Record Today</Link>
+          <Link to="/wellness_data">Dashboard</Link>
+          <Link to="/user_shares">My Inner Circle</Link>
         </>
       )}
+
+      {!currentUser ? (
+        <>
+          <div><Link to="/login">Login</Link></div>
+          <div><Link to="/signup">Sign Up</Link></div>
+        </>
+      ) : (
+          <button onClick={handleLogout}>Sign Out</button>
+      )}
     </div>
-  )
+  );
 }
