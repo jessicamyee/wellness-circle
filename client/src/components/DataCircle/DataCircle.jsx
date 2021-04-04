32903d64cdd01a59661951afe2be80cdbfdb3c9c
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-04-04 15:04:02 -0400

    add hover to buttons

--- client/src/components/DataCircle/DataCircle.jsx
+++ client/src/components/DataCircle/DataCircle.jsx
@@ -5,7 +5,7 @@ export default function DataCircle({ title, content, colorClass }) {
   return (
     <div className={`data-circle-individual ${colorClass}`}>
       <div id="circle-title">{title}</div>
-      <div id="circle-content">{content}</div>
+      <div id="circle-content">{content || "N/A"}</div>
     </div>
   );
 }
